de36169b708043d7b4baf6c424121236b4129c4f
fulber20 <1527029@senati.pe>
2024-08-21 19:28:22 +0200

    mi comentario4

--- prom3.py
+++ prom3.py
@@ -1,6 +1,4 @@
-n1="b"
-n2="c"
-int(input("ingresa el primer numero"))
-int(input("ingresa el segundo numero"))
+n1=int(input("ingresa el primer numero"))
+n2=int(input("ingresa el segundo numero"))
 r=n1+n2
 print("la suma es",r),
