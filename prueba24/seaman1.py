c10130b5c7053566e5dad7ad982b9ff6d67e9d25
FULBER PONCE <1527029@senati.pe>
2024-08-30 17:28:35 -0500

    actualizar

--- prueba24/seaman1.py
+++ prueba24/seaman1.py
@@ -1,13 +1,34 @@
-import pandas as pd 
-import numpy as np
-
-data = {
-    
-    'Curso':['SUPERVISOR DE TRABAJOS DE ALTO RIESGO', 'TRABAJOS EN CALIENTE','TRABAJOS EN ALTURA','TRABAJOS ELÉCTRICOS','METRADOS EN EDIFICACIONES','ELABORACION DE COSTOS Y PRESUPUESTOS DE OBRA','PROGRAMACIÓN Y CONTROL DE OBRAS'],
-    'DNI':[45592022, 45592022, 45592022, 45592022, 45099189, 45099189],	
-    'Nombres y Apellidos':['NATHALY MILAGROS CARDENAS CARPIO','NATHALY MILAGROS CARDENAS CARPIO','NATHALY MILAGROS CARDENAS CARPIO','NATHALY MILAGROS CARDENAS CARPIO','JOSE EDUARDO HINOSTROZA ARAUJO','JOSE EDUARDO HINOSTROZA ARAUJO'],
-    'F. Inicio':['9/11/2023','7/02/2024','2/03/2024', '5/04/2024', '5/09/2023', '6/10/2023', '4/11/2023'],
-    'F. Final':['29/04/2024', '28/02/2024', '28/03/2024', '27/04/2024', '31/01/2024', '28/02/2024', '30/03/2024' ],
+import pandas as pd
+import numpy as np 
+#datos de la empresa
+Date = {
+    'Curso': [
+        'SUPERVISOR DE TRABAJOS DE ALTO RIESGO',
+        'TRABAJOS EN CALIENTE',
+        'TRABAJOS EN ALTURA',
+        'TRABAJOS ELÉCTRICOS',
+        'METRADOS EN EDIFICACIONES',
+        'ELABORACION DE COSTOS Y PRESUPUESTOS DE OBRA',
+        'PROGRAMACIÓN Y CONTROL DE OBRAS'
+    ],
+    'DNI': [45592328, 45592328, 45592328, 45592328, 45089649, 45089649,  45089649],
+    'Nombres ': [
+        'NATHALY  CARPIO',
+        'NATHALY  CARPIO',
+        'NATHALY  CARPIO',
+        'NATHALY CARDENAS',
+        'JOSE HINOSTROZA',
+        'JOSE HINOSTROZA',
+        'JOSE EDUARDO'
+    ],
+    'F. Inicio': ['2023-09-11', '2024-02-07', '2024-03-02', '2024-04-05', '2023-09-05', '2023-10-06', '2023-11-04'],
+    'F. Final': ['2024-04-29', '2024-02-28', '2024-03-28', '2024-04-27', '2024-01-31', '2024-02-28', '2024-03-30']
 }
-df = pd.DataFrame(data)
+
+df = pd.DataFrame(Date)
+
+# Convertir las columnas de fecha a formato datetime
+df['F. Inicio'] = pd.to_datetime(df['F. Inicio'], format='%Y-%m-%d')
+df['F. Final'] = pd.to_datetime(df['F. Final'], format='%Y-%m-%d')
+
 print(df)
